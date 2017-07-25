83c0ea89a388dc9738f0da021eb094fa14213030
한유덕 <Duck@han-yudeog-ui-MacBook-Pro.local>
2017-07-25 21:03:00 +0900

    템플릿 적용

---
+++ app/controllers/foreigner_controller.rb
@@ -0,0 +1,32 @@
+class ForeignerController < ApplicationController
+
+  ##CREATE
+  def new
+  end
+
+  def create
+  end
+
+
+  ##READ
+  def show
+  end
+
+  def index
+  end
+
+
+  ##UPDATE
+  def edit
+  end
+
+  def update
+  end
+
+
+  ##DELETE
+  def destroy
+  end
+
+
+end
