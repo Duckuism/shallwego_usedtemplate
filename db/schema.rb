2d0695fde04ad4d5dc1353e3abc3a9afef4a99bf
BHPark <jjbs0408@gmail.com>
2017-08-07 15:54:48 +0900

    회원가입

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,27 @@
 
 ActiveRecord::Schema.define(version: 20170806080727) do
 
+  create_table "courses", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "image"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer  "limit_person"
+    t.string   "course_summary"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "place_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_locations_on_course_id"
+  end
+
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
     t.string  "unsubscriber_type"
     t.integer "unsubscriber_id"
@@ -69,6 +90,12 @@ ActiveRecord::Schema.define(version: 20170806080727) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name",                   default: "", null: false
+    t.string   "gender"
+    t.string   "from"
+    t.string   "phone"
+    t.text     "introduce"
+    t.string   "image"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -77,32 +104,19 @@ ActiveRecord::Schema.define(version: 20170806080727) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-ActiveRecord::Schema.define(version: 20170731052806) do
-
-  create_table "courses", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.string   "image"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.integer  "limit_person"
-    t.string   "course_summary"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "name"
-    t.string   "place_id"
-    t.integer  "course_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["course_id"], name: "index_locations_on_course_id"
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
