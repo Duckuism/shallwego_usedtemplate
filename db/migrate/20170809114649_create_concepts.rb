47a57dc2ad11a8e82d10e0f30b65bc67498bf605
한유덕 <Duck@han-yudeog-ui-MacBook-Pro.local>
2017-08-18 16:29:26 +0900

    hack your life sibal

--- db/migrate/20170809114649_create_concepts.rb
+++ db/migrate/20170809114649_create_concepts.rb
@@ -14,6 +14,7 @@ class CreateConcepts < ActiveRecord::Migration[5.0]
       t.string :hangout_type
       t.string :location
       t.belongs_to :user
+      t.string :image
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170811103800) do
     t.string   "hangout_type"
     t.string   "location"
     t.integer  "user_id"
+    t.string   "image"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.index ["user_id"], name: "index_concepts_on_user_id"
